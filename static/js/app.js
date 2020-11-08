07942dfc77fc1aa197d3cffe777ce51a079bf06a
Zachariassis <Zachariassis@gmail.com>
2020-11-08 12:11:11 -0600

    Level 2 Complete

--- static/js/app.js
+++ static/js/app.js
@@ -11,10 +11,34 @@ button.on("click", runEnter);
 function runEnter() {
     d3.event.preventDefault();
 
-    var inputElement = d3.select('#datetime').property('value')
-    console.log(inputElement)
+    var inputElement1 = d3.select('#datetime').property('value')
+    var inputElement2 = d3.select('#city').property('value')
+    var inputElement3 = d3.select('#state').property('value')
+    var inputElement4 = d3.select('#country').property('value')
+    var inputElement5 = d3.select('#shape').property('value')
 
-    var filteredData = tableData.filter(dat => dat.datetime ===inputElement)
+    var filteredData = tableData
+
+    if(inputElement1 !== null && inputElement1 !== '') {
+        filteredData = filteredData.filter(dat => dat.datetime ===inputElement1)
+        console.log(inputElement1)
+    }
+    if(inputElement2 !== null && inputElement2 !== '') {
+        filteredData = filteredData.filter(dat => dat.city ===inputElement2)
+        console.log(inputElement2)
+    }
+    if(inputElement3 !== null && inputElement3 !== '') {
+        filteredData = filteredData.filter(dat => dat.state ===inputElement3)
+        console.log(inputElement3)
+    }
+    if(inputElement4 !== null && inputElement4 !== '') {
+        filteredData = filteredData.filter(dat => dat.country ===inputElement4)
+        console.log(inputElement4)
+    }
+    if(inputElement5 !== null && inputElement5 !== '') {
+        filteredData = filteredData.filter(dat => dat.shape ===inputElement5)
+        console.log(inputElement5)
+    }
 
     var list = d3.select("#data-here")
     list.html("")
@@ -32,7 +56,7 @@ function runEnter() {
         var listName='Item-'+i
         list.append('tr').attr('id', listName)
         var row = d3.select('#'+listName)
-        console.log(row)
+        //console.log(row)
         row.append('th').text(filteredData[i].datetime)
         row.append('th').text(filteredData[i].city)
         row.append('th').text(filteredData[i].state)
